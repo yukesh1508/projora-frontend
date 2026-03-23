140a3a532bbb5525176f74bedca0bd0c270ff5d7
Yukesh S <sekarj170@gmail.com>
2026-03-23 19:10:38 +0530

    frontend update

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://projora-backend-ub37.onrender.com";
+const API_BASE_URL = Process.env.REACT_APP_API_URL || "https://projora-backend-ub37.onrender.com";
 
 export default API_BASE_URL;
